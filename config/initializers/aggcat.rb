766a45ff428600759b2d93db10a36050d986718b
Gallagher Polyn <gallagher.polyn@gmail.com>
2016-02-26 17:12:49 -0500

    bunch of changes

--- config/initializers/aggcat.rb
+++ config/initializers/aggcat.rb
@@ -1,8 +1,10 @@
-Rails.logger.info "FART"
 Aggcat.configure do |config|
   config.issuer_id = Rails.application.secrets.intuit_saml_id
   config.consumer_key = Rails.application.secrets.intuit_consumer_id
   config.consumer_secret = Rails.application.secrets.intuit_consumer_secret
-  # config.certificate_value = Rails.application.secrets.intuit_cert_value
-  config.certificate_path = Rails.application.secrets.config_cert_path
+  if Rails.application.secrets.intuit_cert_value.nil?
+    config.certificate_path = Rails.application.secrets.config_cert_path
+  else
+    config.certificate_value = Rails.application.secrets.intuit_cert_value
+  end
 end
